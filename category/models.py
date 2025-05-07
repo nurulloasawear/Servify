9bdfcfd54fc575ed89dca5949421c42faf63b722
ruhulloh <153582843+ruhulloh@users.noreply.github.com>
2025-05-07 12:11:10 +0500

    admin, auth, category, contenttypes, proffessions, sessions, users

--- category/models.py
+++ category/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    is_main = models.BooleanField(default=False)
+    parent = models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True,related_name="subcategorries")
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return f"{self.name} main:{self.is_main}"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import User as BaseUserManager
 # Create your models here.
+class Users(AbstractBaseUser):
+    roles = [("is_Admin",0),('is_specialist',1),('is_client',2)]
+    role = models.SmallIntegerField(choices=roles,default=2)
+    full_name = models.CharField(max_length=50,null=False,blank=False)
+    phone = models.CharField(null=False,max_length=30,unique=True)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return f"id:{self.id} - {self.full_name}"
+class Profile(models.Model):
+    user = models.OneToOneField(BaseUserManager,on_delete=models.CASCADE)
+    
+class user_interesting_analyz(models.Model):
+    user = models.OneToOneField(BaseUserManager,on_delete=models.CASCADE)
+ 
+class user_action_time_set_ad(models.Model):
+    user = models.OneToOneField(BaseUserManager,on_delete=models.CASCADE)
+
