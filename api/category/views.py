5bbb332ccfe360198004b9f5cfdae0aba9d82f63
nurulloasawear <nurullostepn3@gmail.com>
2025-05-10 08:48:05 +0500

    Finaly 0 errors

--- api/category/views.py
+++ api/category/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from category.models import Category
 from .serializers import * 
-
+from api.permissions import IsAdminOrReadOnly
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes =[IsAdminOrReadOnly]
+    permission_classes = (IsAdminOrReadOnly)
 
 
--- api/location/views.py
+++ api/location/views.py
@@ -2,10 +2,9 @@ from rest_framework import viewsets
 from location.models import Location
 from .serializers import LocationSerializer
 from .serializers import LocationSerializer
-
+from api.permissions import IsClient,IsSpecialist
 
 class LocationViewSet(viewsets.ModelViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
     permission_classes =[IsClient,IsSpecialist]
-    
